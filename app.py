704468c3c114c03413de8454f8c96ef364d8b913
vboxuser <sga18@alumnes.udl.cat>
2026-02-18 18:50:52 +0000

    Pràctica 1: Flask + formulari + SQLite + estàtics

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from database import init_db, guardar_grup, carregar_grup
+app = Flask(__name__)
+init_db()
+@app.route("/", methods=["GET", "POST"])
+def index():
+	if request.method == "POST":
+		noms_text = request.form.get("noms")
+		if noms_text:
+			grup = [nom.strip() for nom in noms_text.split(",")]
+			guardar_grup(grup)
+	grup = carregar_grup()
+	return render_template("index.html", grup=grup)
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=8000, debug=True)
