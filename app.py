6d2ce28492454610ad3e06eb3916a14113a9a432
Alex-0Esc <aep11@alumnes.udl.cat>
2026-02-19 18:19:17 +0100

    Cambio trivial de cojones

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def index():
 	grup = carregar_grup()
 	return render_template("index.html", grup=grup)
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000, debug=True)
+	app.run(host="0.0.0.0", port=8000, debug=False)
