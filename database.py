54b9dcf1037f7f3c4f725f8938e052fe57a2f1e2
samuel <sga18@alumnes.udl.cat>
2026-03-05 09:25:26 +0100

    Comentarios

--- database.py
+++ database.py
@@ -2,12 +2,14 @@ import sqlite3
 DB_NAME = "grup.db"
 def get_connection():
 	return sqlite3.connect(DB_NAME)
+# Inicializamos la base de datos
 def init_db():
 	conn = get_connection()
 	cursor = conn.cursor()
 	cursor.execute("""CREATE TABLE IF NOT EXISTS grup (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT NOT NULL)""")
 	conn.commit()
 	conn.close()
+# Funcion que guarda los nombres que introducimos
 def guardar_grup(noms):
 	conn = get_connection()
 	cursor = conn.cursor()
@@ -16,6 +18,7 @@ def guardar_grup(noms):
 		cursor.execute("INSERT INTO grup (nom) VALUES (?)", (nom,))
 	conn.commit()
 	conn.close()
+# Funcion opara mostrar los nombres introducidos
 def carregar_grup():
 	conn = get_connection()
 	cursor = conn.cursor()
