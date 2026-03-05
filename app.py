c9770eb66ce150005c03fcad2bb7c33d2c8c42b0
samuel <sga18@alumnes.udl.cat>
2026-03-05 09:20:31 +0100

    Comentarios

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 from database import init_db, guardar_grup, carregar_grup
+# Creem aplicació
 app = Flask(__name__)
 init_db()
+# Definim endpoint y el tipus de peticions que s'accepten
 @app.route("/", methods=["GET", "POST"])
 def index():
+	# Gestionem 
 	if request.method == "POST":
 		noms_text = request.form.get("noms")
 		if noms_text:
@@ -11,5 +14,6 @@ def index():
 			guardar_grup(grup)
 	grup = carregar_grup()
 	return render_template("index.html", grup=grup)
+# Le decimos a la app como desplegarse, por donde escuchar las peticiones
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80, debug=False)
